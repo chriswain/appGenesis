c8262eb2e86ed6ec2ec6e3ce928f9ade08e1ce0d
chriswain <chriswainwrightaaron@gmail.com>
2015-05-23 01:20:20 -0400

    almost complete

--- MyWorld/MyWorld/ViewController.swift
+++ MyWorld/MyWorld/ViewController.swift
@@ -148,17 +148,72 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         
         detailVC.navigationItem.title = venue["name"] as? String
         
-        var staticCategoryLabel = UILabel(frame: CGRectMake(100, 80, 100, 20))
+        var staticCategoryLabel = UILabel(frame: CGRectMake(100, 80, 180, 20))
         staticCategoryLabel.textColor = UIColor.grayColor()
         staticCategoryLabel.text = "Category"
         
+        var bar1 = UILabel(frame: CGRectMake(80, 80, 7, 70))
+        bar1.backgroundColor = UIColor(red:0.69, green:0.69, blue:0.69, alpha:1)
+        
+        detailVC.view.addSubview(bar1)
         detailVC.view.addSubview(staticCategoryLabel)
         
-        var staticAddressLabel = UILabel(frame: CGRectMake(100, 180, 100, 20))
+        var staticAddressLabel = UILabel(frame: CGRectMake(100, 180, 180, 20))
         staticAddressLabel.text = "Address"
         staticAddressLabel.textColor = UIColor.grayColor()
         
+        var bar2 = UILabel(frame: CGRectMake(80, 180, 7, 220))
+        bar2.backgroundColor = UIColor(red:0.69, green:0.69, blue:0.69, alpha:1)
+        
+        detailVC.view.addSubview(bar2)
         detailVC.view.addSubview(staticAddressLabel)
+        
+        var staticHereNowLabel = UILabel(frame: CGRectMake(100, 450, 180, 20))
+        staticHereNowLabel.text = "HereNow"
+        staticHereNowLabel.textColor = UIColor.grayColor()
+        
+        var bar3 = UILabel(frame: CGRectMake(80, 450, 7, 60))
+        bar3.backgroundColor = UIColor(red:0.69, green:0.69, blue:0.69, alpha:1)
+        
+        detailVC.view.addSubview(staticHereNowLabel)
+        detailVC.view.addSubview(bar3)
+        
+        var statsLabel = UILabel(frame: CGRectMake(100, 280, 50, 20))
+        statsLabel.text = "Stats"
+        statsLabel.textColor = UIColor.grayColor()
+        
+        var statCircleUsers = UILabel(frame:CGRectMake(100, 320, 50, 50))
+        statCircleUsers.layer.cornerRadius = 25
+        statCircleUsers.backgroundColor = UIColor(red:0.69, green:0.69, blue:0.69, alpha:1)
+        statCircleUsers.clipsToBounds = true
+        statCircleUsers.textColor = UIColor.whiteColor()
+        
+        var staticUsersLabel = UILabel(frame: CGRectMake(105, 380, 100, 20))
+        staticUsersLabel.text = "USERS"
+        staticUsersLabel.textColor = UIColor.grayColor()
+        staticUsersLabel.font = UIFont(name: "helvetica neue", size: 12)
+        
+        var statCircleCheckins = UILabel(frame: CGRectMake(160, 320, 50, 50))
+        statCircleCheckins.layer.cornerRadius = 25
+        statCircleCheckins.backgroundColor = UIColor(red:0.69, green:0.69, blue:0.69, alpha:1)
+        statCircleCheckins.clipsToBounds = true
+        statCircleCheckins.textColor = UIColor.whiteColor()
+        
+        var staticCheckinsLabel = UILabel(frame: CGRectMake(160, 380, 100, 20))
+        staticCheckinsLabel.text = "CHECKINS"
+        staticCheckinsLabel.textColor = UIColor.grayColor()
+        staticCheckinsLabel.font = UIFont(name: "helvetica neue", size: 12)
+        
+        var statCircleTips = UILabel(frame: CGRectMake(220, 320, 50, 50))
+        statCircleTips.layer.cornerRadius = 25
+        statCircleTips.backgroundColor = UIColor(red:0.69, green:0.69, blue:0.69, alpha:1)
+        statCircleTips.clipsToBounds = true
+        statCircleTips.textColor = UIColor.whiteColor()
+        
+        var staticTipsLabel = UILabel(frame: CGRectMake(233, 380, 100, 20))
+        staticTipsLabel.text = "TIPS"
+        staticTipsLabel.textColor = UIColor.grayColor()
+        staticTipsLabel.font = UIFont(name: "helvetica neue", size: 12)
         
         if let location = venue["location"] as? [String:AnyObject] {
             
@@ -174,11 +229,41 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
             
             var dynamicCategoryLabel = UILabel(frame: CGRectMake(100, 100, 100, 40))
             let insideCategoriesArray = categories[0]
-            let categoriesName = insideCategoriesArray["name"]
+            let categoriesName: AnyObject? = insideCategoriesArray["name"]
             dynamicCategoryLabel.text = categoriesName as? String
             
             detailVC.view.addSubview(dynamicCategoryLabel)
         }
+        
+        if let stats = venue["stats"] as? [String:AnyObject] {
+            statCircleUsers.text = stats["usersCount"] as? String
+            statCircleCheckins.text = stats["checkinsCount"] as? String
+            statCircleTips.text = stats["tipCount"] as? String
+        }
+        
+        detailVC.view.addSubview(statsLabel)
+        detailVC.view.addSubview(statCircleUsers)
+        detailVC.view.addSubview(staticUsersLabel)
+        detailVC.view.addSubview(statCircleCheckins)
+        detailVC.view.addSubview(staticCheckinsLabel)
+        detailVC.view.addSubview(statCircleTips)
+        detailVC.view.addSubview(staticTipsLabel)
+        
+        if let hereNow = venue["hereNow"] as? [String:AnyObject] {
+            var hereNowLabel = UILabel(frame: CGRectMake(100, 475, 150, 40))
+            hereNowLabel.text = hereNow["summary"] as? String
+            
+            detailVC.view.addSubview(hereNowLabel)
+        }
+        
+        if let icon = venue["categories"] as? [[String:AnyObject]] {
+            var iconPic = UILabel(frame: CGRectMake(40, 80, 40, 40))
+            let inCategoriesArray = icon[0]
+            let iconImage: AnyObject? = inCategoriesArray["icon"]
+ //           iconPic.addSubview(iconImage) as UIImage
+            }
+            
+        
         
         
         
